34b7579b7980555b0342a027c338e6701070733a
IssyisBusy <122068899+IssyisBusy@users.noreply.github.com>
2023-09-06 22:42:29 +1000

    Create PsModules.ps1

---
+++ PsModules.ps1
@@ -0,0 +1,15 @@
+Install-Module -Name NuGet -Confirm
+Install-Module -Name Az.Accounts -Confirm
+Install-Module -Name Az.Resources -Confirm
+Install-Module -Name Az.KeyVault -Confirm
+Install-Module -Name Az.Network -Confirm
+Install-Module -Name powershell-yaml -Confirm
+Install-Module -Name AzureAD -Confirm
+Install-Module -Name SharePointPnPPowerShellOnline -Confirm
+Install-Module -Name SNMP -Confirm
+Install-Module -Name ExchangeOnlineManagement -AllowPrerelease -Confirm
+Install-Module -Name MSOnline -Confirm
+Install-Module -Name Microsoft.Graph.Authentication -Confirm
+Install-Module -Name Microsoft.Graph.Intune -Confirm
+Install-Module -Name PnP.PowerShell -AllowPrerelease -Confirm
+Install-Module -Name NuGet -Confirm
