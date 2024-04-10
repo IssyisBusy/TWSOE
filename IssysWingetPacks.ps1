86a7d60dadc7f2321e60de803ddfc34572b8be19
Israel Bishop <122068899+IssyisBusy@users.noreply.github.com>
2024-04-10 11:41:42 +1000

    Updated apps and fixed formatting error

--- IssysWingetPacks.ps1
+++ IssysWingetPacks.ps1
@@ -1,46 +1,47 @@
-Write-Output "Installing Additional Apps"
+Write-Output "Installing Apps"
 $apps = @(
-  @{name = "7zip.7zip" },
-  @{name = "Adobe.Acrobat.Reader.64-bit" },
-  @{name = "VideoLAN.VLC" },
-  @{name = "Google.Chrome" },
-  @{name = "Microsoft.Office" },
-	@{name = "Microsoft.PowerToys" },
-	@{name = "Microsoft.Teams" },
-	@{name = "Microsoft.OneDrive" },
-	@{name = "Microsoft.VisualStudioCode" },
-	@{name = "JohnMacFarlane.Pandoc" },
-	@{name = "OpenJS.NodeJS" },
-	@{name = "dotPDNLLC.paintdotnet" },
-	@{name = "Famatech.AdvancedIPScanner" },
-	@{name = "Git.Git" },
-	@{name = "AutoHotkey.AutoHotkey" },
-	@{name = "Insecure.Nmap" },
-	@{name = "Insecure.Npcap" },
-	@{name = "WiresharkFoundation.Wireshark" },
-	@{name = "JanDeDobbeleer.OhMyPosh" },
-	@{name = "RingCentral.RingCentral" },
-	@{name = "GitHub.GitHubDesktop" },
-	@{name = "JohnMacFarlane.Pandoc" },
-	@{name = "OpenJS.NodeJS" },
-	@{name = "dotPDNLLC.paintdotnet" },
-	@{name = "Famatech.AdvancedIPScanner" },
-	@{name = "Git.Git" },
-	@{name = "AutoHotkey.AutoHotkey" },
-	@{name = "Insecure.Nmap" },
-	@{name = "Insecure.Npcap" },
-	@{name = "WiresharkFoundation.Wireshark" },
-	@{name = "JanDeDobbeleer.OhMyPosh" },
-	@{name = "RingCentral.RingCentral" },
-	@{name = "Microsoft.NuGet" },
-	@{name = "Obsidian.Obsidian" }
+    @{name = "7zip.7zip" },
+    @{name = "Adobe.Acrobat.Reader.64-bit" },
+    @{name = "VideoLAN.VLC" },
+    @{name = "Google.Chrome" },
+    @{name = "Microsoft.Office" },
+    @{name = "Microsoft.PowerToys" },
+    @{name = "Microsoft.Teams" },
+    @{name = "Microsoft.OneDrive" },
+    @{name = "Microsoft.VisualStudioCode" },
+    @{name = "JohnMacFarlane.Pandoc" },
+    @{name = "OpenJS.NodeJS" },
+    @{name = "dotPDNLLC.paintdotnet" },
+    @{name = "Famatech.AdvancedIPScanner" },
+    @{name = "Git.Git" },
+    @{name = "AutoHotkey.AutoHotkey" },
+    @{name = "Insecure.Nmap" },
+    @{name = "Insecure.Npcap" },
+    @{name = "WiresharkFoundation.Wireshark" },
+    @{name = "JanDeDobbeleer.OhMyPosh" },
+    @{name = "RingCentral.RingCentral" },
+    @{name = "GitHub.GitHubDesktop" },
+    @{name = "JohnMacFarlane.Pandoc" },
+    @{name = "OpenJS.NodeJS" },
+    @{name = "dotPDNLLC.paintdotnet" },
+    @{name = "Famatech.AdvancedIPScanner" },
+    @{name = "Git.Git" },
+    @{name = "AutoHotkey.AutoHotkey" },
+    @{name = "Insecure.Nmap" },
+    @{name = "Insecure.Npcap" },
+    @{name = "WiresharkFoundation.Wireshark" },
+    @{name = "JanDeDobbeleer.OhMyPosh" },
+    @{name = "RingCentral.RingCentral" },
+    @{name = "Microsoft.NuGet" },
+    @{name = "Obsidian.Obsidian" }
 );
 Foreach ($app in $apps) {
     $listApp = winget list --exact -q $app.name
     if (![String]::Join("", $listApp).Contains($app.name)) {
         Write-host "Installing: " $app.name
-        winget install -e -h --accept-source-agreements --accept-package-agreements --disable-interactivity --id $app.name 
+        winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
     }
     else {
         Write-host "Skipping: " $app.name " (already installed)"
     }
+}
